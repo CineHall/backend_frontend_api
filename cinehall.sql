3bb9fe26935ac859bc7dcf8371938d0d99172834
marouane <102564698+marouane216@users.noreply.github.com>
2023-01-31 16:17:57 +0100

    Add files via upload

---
+++ cinehall.sql
@@ -0,0 +1,316 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 31 jan. 2023 à 16:17
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `cinehall`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `film`
+--
+
+CREATE TABLE `film` (
+  `id_f` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `prix` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id_r` int(11) NOT NULL,
+  `id_s` int(11) NOT NULL,
+  `id_f` int(11) NOT NULL,
+  `id_u` int(11) NOT NULL,
+  `date_r` date NOT NULL,
+  `nbr_place` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle1`
+--
+
+CREATE TABLE `salle1` (
+  `id_p` int(11) NOT NULL,
+  `numero_p` int(11) NOT NULL,
+  `reserve` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 = non resrver  |  1 = reserver'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `salle1`
+--
+
+INSERT INTO `salle1` (`id_p`, `numero_p`, `reserve`) VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 0),
+(4, 4, 0),
+(5, 5, 0),
+(6, 6, 0),
+(7, 7, 0),
+(8, 8, 0),
+(9, 9, 0),
+(10, 10, 0),
+(11, 11, 0),
+(12, 12, 0),
+(13, 13, 0),
+(14, 14, 0),
+(15, 15, 0),
+(16, 16, 0),
+(17, 17, 0),
+(18, 18, 0),
+(19, 19, 0),
+(20, 20, 0),
+(21, 21, 0),
+(22, 22, 0),
+(23, 23, 0),
+(24, 24, 0),
+(25, 25, 0),
+(26, 26, 0),
+(27, 27, 0),
+(28, 28, 0),
+(29, 29, 0),
+(30, 30, 0),
+(31, 31, 0),
+(32, 32, 0),
+(33, 33, 0),
+(34, 34, 0),
+(35, 35, 0),
+(36, 36, 0),
+(37, 37, 0),
+(38, 38, 0),
+(39, 39, 0),
+(40, 40, 0),
+(41, 41, 0),
+(42, 42, 0),
+(43, 43, 0),
+(44, 44, 0),
+(45, 45, 0),
+(46, 46, 0),
+(47, 47, 0),
+(48, 48, 0),
+(49, 49, 0),
+(50, 50, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle2`
+--
+
+CREATE TABLE `salle2` (
+  `id_p` int(11) NOT NULL,
+  `numero_p` int(11) NOT NULL,
+  `reserve` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 = non resrver  |  1 = reserver'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `salle2`
+--
+
+INSERT INTO `salle2` (`id_p`, `numero_p`, `reserve`) VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 0),
+(4, 4, 0),
+(5, 5, 0),
+(6, 6, 0),
+(7, 7, 0),
+(8, 8, 0),
+(9, 9, 0),
+(10, 10, 0),
+(11, 11, 0),
+(12, 12, 0),
+(13, 13, 0),
+(14, 14, 0),
+(15, 15, 0),
+(16, 16, 0),
+(17, 17, 0),
+(18, 18, 0),
+(19, 19, 0),
+(20, 20, 0),
+(21, 21, 0),
+(22, 22, 0),
+(23, 23, 0),
+(24, 24, 0),
+(25, 25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle3`
+--
+
+CREATE TABLE `salle3` (
+  `id_p` int(11) NOT NULL,
+  `numero_p` int(11) NOT NULL,
+  `reserve` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 = non resrver  |  1 = reserver'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `salle3`
+--
+
+INSERT INTO `salle3` (`id_p`, `numero_p`, `reserve`) VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 0),
+(4, 4, 0),
+(5, 5, 0),
+(6, 6, 0),
+(7, 7, 0),
+(8, 8, 0),
+(9, 9, 0),
+(10, 10, 0),
+(11, 11, 0),
+(12, 12, 0),
+(13, 13, 0),
+(14, 14, 0),
+(15, 15, 0),
+(16, 16, 0),
+(17, 17, 0),
+(18, 18, 0),
+(19, 19, 0),
+(20, 20, 0),
+(21, 21, 0),
+(22, 22, 0),
+(23, 23, 0),
+(24, 24, 0),
+(25, 25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_u` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `keyLogin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id_f`);
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id_r`),
+  ADD KEY `salle` (`id_s`),
+  ADD KEY `film` (`id_f`),
+  ADD KEY `user` (`id_u`);
+
+--
+-- Index pour la table `salle1`
+--
+ALTER TABLE `salle1`
+  ADD PRIMARY KEY (`id_p`);
+
+--
+-- Index pour la table `salle2`
+--
+ALTER TABLE `salle2`
+  ADD PRIMARY KEY (`id_p`);
+
+--
+-- Index pour la table `salle3`
+--
+ALTER TABLE `salle3`
+  ADD PRIMARY KEY (`id_p`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_u`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id_f` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id_r` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `salle1`
+--
+ALTER TABLE `salle1`
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT pour la table `salle2`
+--
+ALTER TABLE `salle2`
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT pour la table `salle3`
+--
+ALTER TABLE `salle3`
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_u` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `film` FOREIGN KEY (`id_f`) REFERENCES `film` (`id_f`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user` FOREIGN KEY (`id_u`) REFERENCES `users` (`id_u`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
