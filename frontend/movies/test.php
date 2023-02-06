cfa213cfe36098a7dc32143e8a7700b1715ad069
marouane <uanemaro216@gmail.com>
2023-02-06 08:49:57 +0100

    update css

--- frontend/movies/test.php
+++ frontend/movies/test.php
@@ -1,15 +1,3 @@
 <?php include('../inc/test.php') ?>
-<div id="movie">
-    <div class="movie"><img src="http://localhost/backend_frontend_api/backend/img/john_Wike.jpg" alt="image pour john_Wike">
-        <div class="span">
-            <span>john_Wike</span>
-            <span>2 hours</span>
-            <span>60</span>
-            <span>salle 1</span>
-            <div>
-                <a href="#">reserve</a>
-            </div>
-        </div>
-    </div>
-</div>
+
 <?php include('../inc/footer.php') ?>
